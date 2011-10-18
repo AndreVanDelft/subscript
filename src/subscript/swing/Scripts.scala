9257094a48c6cb8bb8713a49e9e0a24d63a9d6b1
andre.vandelft <andre.vandelft@localhost>
2011-10-18 21:53:24 +0200

    Added headers with GPL license

--- src/subscript/swing/Scripts.scala
+++ src/subscript/swing/Scripts.scala
@@ -1,3 +1,29 @@
+/*
+    This file is part of Subscript - an extension of the Scala language 
+                                     with constructs from Process Algebra.
+
+    Subscript is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License and the 
+    GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Subscript consists partly of a "virtual machine". This is a library; 
+    Subscript applications may distribute this library under the 
+    GNU Lesser General Public License, rather than under the 
+    GNU General Public License. This way your applications need not 
+    be made Open Source software, in case you don't want to.
+
+    Subscript is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You may have received a copy of the GNU General Public License
+    and the GNU Lesser General Public License along with Subscript.
+    If not, see <http://www.gnu.org/licenses/>
+*/
+
 package subscript.swing
 import scala.swing._
 import scala.swing.event._
